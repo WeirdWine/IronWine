ac20fd9dcaf1a69b0550f8a5fc1d79857aed74be
Simon Gowing <simongowing1@gmail.com>
2021-02-15 19:04:39 +0100

    problems to solve: username duplication, log in when age is blank

--- wineX/routes/signup.js
+++ wineX/routes/signup.js
@@ -19,6 +19,11 @@ router.post("/signup", (req, res, next) => {
     const thisYear = today.getFullYear()
     const userDateOfBirth = new Date(dateOfBirth).getFullYear()
     
+    if (username === '') {
+        res.render("user/signup", { message: "Your username cannot be empty" });
+        return
+      }
+
     if (password.length < 8) {
         return res.render("user/signup", { message: "Your password must be more than 8 characters"
     });
@@ -27,23 +32,28 @@ router.post("/signup", (req, res, next) => {
     if (userDateOfBirth > (thisYear - 19)) {
         return res.render("user/signup", { message: "You must be of legal drinking age (18+) to enter"
     });
-    } else {res.render("user/login")}
-   
-    //if (dateOfBirth > )
-    
-    User.create({
-        username: username,
-        password: hash,
-        email: email,
-        dateOfBirth: dateOfBirth,
-        imgPath: imgPath})
-    .then(newUser => {
-        
-        res.render("user/login")
-    })
-    .catch(err => {
-        next(err);
-    })
+    } 
+
+    User.findOne({ username: username })
+    .then(userFromDB => {
+      if (userFromDB !== null) {
+        res.render("user/login", { message: "Username is already taken" });
+      } else {
+        User.create({
+            username: username,
+            password: hash,
+            email: email,
+            dateOfBirth: dateOfBirth,
+            imgPath: imgPath})
+        .then(newUser => {
+            
+            res.render("user/login", {name: newUser})
+        })
+        .catch(err => {
+            next(err);
+        })
+      }
+})
 })
 
 module.exports = router;

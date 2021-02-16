a318102c29df398c3daa76ff6b13caf3fb1f78c7
Adam Tomczyk <markus.adam.tomczyk@gmail.com>
2021-02-16 16:22:35 +0100

    Add Wine / Wines work, minor error in the form file, emty data

--- wineX/models/Wine.js
+++ wineX/models/Wine.js
@@ -7,21 +7,22 @@ const wineSchema = new Schema({
       winename: String,
       year: String,
       price: Number,
-      colour: {
-        type: String,
-        enum: ['white', 'red', 'rose', 'orange']
-      },
+      colour: String,
       bio: Boolean,
       country: String,
       imgName: String,
       imgPath: String,
       publicId: String,
+      cheese: String,
       owner: [{type: Schema.Types.ObjectId, ref: User}],
       shop: [{type: Schema.Types.ObjectId, ref: Shop}],
+     
       comments: [{
-        user: {type: Schema.Types.ObjectId, ref: User},
-        comment: String
+       // user: {type: Schema.Types.ObjectId, ref: User},
+        comments: String
       }]
+      
+   
        
 });
 

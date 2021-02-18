aec38bf27dd9d05b8c60b5624a3f7f7b2b2ae06d
Simon Gowing <simongowing1@gmail.com>
2021-02-18 16:29:14 +0100

    no mapbox :(

--- public/js/script.js
+++ public/js/script.js
@@ -8,6 +8,18 @@ document.addEventListener(
 
 );
 
+/*
+mapboxgl.accessToken = 'pk.eyJ1IjoidGljaHUwMCIsImEiOiJja2w4YnB6MXYwcGZzMnJyMnJkM3lpOXI1In0.o3phVJgL6V3TtAa7JiGkBw';
+var geocoder = new MapboxGeocoder({
+accessToken: mapboxgl.accessToken,
+bbox: [13.274049, 52.452221, 13.522614, 52.565059],
+  proximity: {
+    longitude: 13.404954,
+    latitude: 52.520008
+  }
+});
+*/
 
+geocoder.addTo('#geocoder');
 
 
